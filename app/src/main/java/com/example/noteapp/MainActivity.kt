ecb07e778f6ed9f70228b8a29690989840ba65b4
PetrGrebnev <awenberg@yandex.ru>
2022-04-15 22:23:39 +0300

    created a toolbar

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -3,11 +3,39 @@ package com.example.noteapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var mToolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navMenu: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mToolbar = findViewById(R.id.m_toolbar)
+        setSupportActionBar(mToolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navMenu = findViewById(R.id.nav_menu)
+
+        var actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            mToolbar,
+            R.string.navigation_open,
+            R.string.navigation_closed
+        )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        navMenu.setNavigationItemSelectedListener(this)
 
     }
 
@@ -15,6 +43,10 @@ class MainActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
