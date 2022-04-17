923af3432c546762c95722d4bd59bedfd0e851b6
GansA11es <sfsfvl33@mail.ru>
2022-04-17 21:16:22 +0300

    added the text memo creation feature

---
+++ app/src/main/java/com/example/noteapp/TextNoteActivity.kt
@@ -0,0 +1,29 @@
+package com.example.noteapp
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
+class TextNoteActivity : AppCompatActivity() {
+
+    private lateinit var creationPageToolbar: Toolbar
+    private lateinit var creationPageDrawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        creationPageToolbar = findViewById(R.id.creation_page_toolbar)
+        creationPageDrawerLayout = findViewById(R.id.drawer_creation_page)
+
+        setSupportActionBar(creationPageToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.creation_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
